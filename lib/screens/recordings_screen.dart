0f4c0b6e220c976d7b8ee5b50c314f3a21769ec9
Saddam <sddmhossain@786gmail.com>
2024-05-04 21:53:42 +0530

    mk

--- lib/screens/recordings_screen.dart
+++ lib/screens/recordings_screen.dart
@@ -69,24 +69,26 @@ class _RecordingsScreenState extends State<RecordingsScreen> {
                 File(recording).deleteSync();
               });
             },
-            child: Card(
-              margin: const EdgeInsets.fromLTRB(4, 2, 4, 2),
-              child: ListTile(
-                title: Text(
-                  recording.substring(
-                    recording.lastIndexOf('/') + 1,
-                    recording.lastIndexOf('.'),
+            child: Container(
+              child: Card(
+                margin: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                child: ListTile(
+                  title: Text(
+                    recording.substring(
+                      recording.lastIndexOf('/') + 1,
+                      recording.lastIndexOf('.'),
+                    ),
+                    style: TextStyle(
+                      color: kColorScheme.onSecondary,
+                    ),
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  style: TextStyle(
-                    color: kColorScheme.onSecondary,
-                  ),
-                  overflow: TextOverflow.ellipsis,
+                  onTap: () {
+                    debugPrint('File playing: $recording');
+                    AudioPlayer player = AudioPlayer();
+                    player.play(DeviceFileSource(recording));
+                  },
                 ),
-                onTap: () {
-                  debugPrint('File playing: $recording');
-                  AudioPlayer player = AudioPlayer();
-                  player.play(DeviceFileSource(recording));
-                },
               ),
             ),
           );
