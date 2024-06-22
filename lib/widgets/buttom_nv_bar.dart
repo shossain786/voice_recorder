0548df302296bf33d1fbf042e771b20767c623bc
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-06-22 16:44:35 +0530

    update

--- lib/widgets/buttom_nv_bar.dart
+++ lib/widgets/buttom_nv_bar.dart
@@ -27,13 +27,13 @@ class _MyBottomNavBarState extends State<MyBottomNavBar> {
           label: 'Home',
           backgroundColor: kColorScheme.onPrimaryContainer,
         ),
-        BottomNavigationBarItem(
-          icon: const Icon(
-            Icons.lyrics_outlined,
-          ),
-          label: 'Recordings',
-          backgroundColor: kColorScheme.onPrimaryContainer,
-        ),
+        // BottomNavigationBarItem(
+        //   icon: const Icon(
+        //     Icons.lyrics_outlined,
+        //   ),
+        //   label: 'Recordings',
+        //   backgroundColor: kColorScheme.onPrimaryContainer,
+        // ),
         const BottomNavigationBarItem(
           icon: Icon(Icons.video_library),
           label: 'Youtube Naat',
@@ -58,18 +58,18 @@ class _MyBottomNavBarState extends State<MyBottomNavBar> {
     });
     switch (index) {
       case 0:
-        Navigator.pushNamed(context, '/homePage');
+        Navigator.pushReplacementNamed(context, '/homePage');
         break;
+      // case 1:
+      //   Navigator.pushNamed(context, '/recordings');
+      //   break;
       case 1:
-        Navigator.pushNamed(context, '/recordings');
-        break;
-      case 2:
         Navigator.pushNamed(context, '/youtube');
         break;
-      case 3:
+      case 2:
         Navigator.pushNamed(context, '/bayan');
         break;
-      case 4:
+      case 3:
         Navigator.pushNamed(context, '/naatPage');
         break;
     }
