cd980a123374b12c5e4b90a63d66be642e0d2928
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-06-22 19:41:15 +0530

    code update

--- lib/screens/record_play_screen.dart
+++ lib/screens/record_play_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:audio_waveforms/audio_waveforms.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
@@ -16,12 +17,34 @@ class _PlayScreenState extends State<PlayScreen> {
   late PlayerController playController;
   bool _isPlaying = true;
   late AudioPlayer player;
+  Duration currentPosition = Duration.zero;
+  Duration totalDuration = Duration.zero;
 
   @override
   void initState() {
     playController = PlayerController();
     player = AudioPlayer();
     playRecording(widget.filePath);
+
+    // Add listener for when the audio completes
+    player.onPlayerComplete.listen((_) {
+      resetController();
+    });
+
+    // Add listener for position updates
+    player.onPositionChanged.listen((position) {
+      setState(() {
+        currentPosition = position;
+      });
+    });
+
+    // Add listener for duration updates
+    player.onDurationChanged.listen((duration) {
+      setState(() {
+        totalDuration = duration;
+      });
+    });
+
     super.initState();
   }
 
@@ -50,6 +73,15 @@ class _PlayScreenState extends State<PlayScreen> {
     });
   }
 
+  void resetController() {
+    setState(() {
+      _isPlaying = false;
+      currentPosition = Duration.zero;
+    });
+    playController.release();
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,14 +93,66 @@ class _PlayScreenState extends State<PlayScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                widget.filePath.split('/').last.split('.').first,
-                style: const TextStyle(
-                  fontSize: 30,
+              const SizedBox(height: 30),
+              SizedBox(
+                // width: 250.0,
+                child: DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 50.0,
+                    fontFamily: 'Canterbury',
+                    color: Colors.red,
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      ScaleAnimatedText(
+                        widget.filePath
+                            .split('/')
+                            .last
+                            .split('.')
+                            .first
+                            .toUpperCase(),
+                      ),
+                    ],
+                    repeatForever: true,
+                    stopPauseOnTap: false,
+                    onTap: () {},
+                  ),
                 ),
-                overflow: TextOverflow.ellipsis,
               ),
-              const SizedBox(height: 100),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: [
+                    StreamBuilder<Duration>(
+                      stream: player.onPositionChanged,
+                      builder: (context, snapshot) {
+                        return Slider(
+                          min: 0.0,
+                          max: totalDuration.inMilliseconds.toDouble(),
+                          value: currentPosition.inMilliseconds.toDouble(),
+                          onChanged: (value) {
+                            player.seek(Duration(milliseconds: value.toInt()));
+                          },
+                        );
+                      },
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          formatDuration(currentPosition),
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          formatDuration(totalDuration),
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -116,10 +200,17 @@ class _PlayScreenState extends State<PlayScreen> {
                       : const Text(''),
                 ],
               ),
+              const SizedBox(height: 30),
             ],
           ),
         ),
       ),
     );
+  }
+
+  String formatDuration(Duration duration) {
+    final minutes = duration.inMinutes.remainder(60).toString().padLeft(2, '0');
+    final seconds = duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+    return '$minutes:$seconds';
   }
 }
